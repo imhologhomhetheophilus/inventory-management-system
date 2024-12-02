eaef0ca1e4ffc71bb4bcb43b79c836d3816ed183
job jacob <81748427+imhologhomhetheophilus@users.noreply.github.com>
2024-12-02 15:36:20 +0100

    Update register.php

--- register.php
+++ register.php
@@ -14,7 +14,7 @@
  </head>
 <body>
 <div class="overlay"><div class="loader"></div></div>
-	<!-- Navbar -->
+	<!-- Navbar
 	<?php include_once("./templates/header.php"); ?>
 	<br/><br/>
 	<div class="container">
